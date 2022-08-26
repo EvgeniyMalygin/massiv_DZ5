f1677dc7a69ccb33d0cee29b51dcaf0b8b594cf7
Evgeniy Malygin <black2001@inbox.ru>
2022-08-26 13:13:58 +0300

    Выполнено задание 3

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         // Задание 1
         System.out.println("================= Задание 1 ==============");
-        int [] integer = new int []{1, 2, 3};
+        int[] integer = new int[]{1, 2, 3};
         double[] drob = {1.57, 7.654, 9.986};
-        String [] str = new String[4];
+        String[] str = new String[4];
         str[0] = "осень";
         str[1] = "зима";
         str[2] = "весна";
@@ -12,37 +12,66 @@ public class Main {
         // Задание 2
         System.out.println("================= Задание 2 ==============");
         System.out.print("Целочисленный массив integer: ");
-        for (int i = 0; i < integer.length; i++)
-        {
-        if (i < integer.length - 1)
-        {
-        System.out.print(integer[i] + ", ");
-        }else
-        {
-            System.out.println(integer[i]);
-        }
+        for (int i = 0; i < integer.length; i++) {
+            if (i < integer.length - 1) {
+                System.out.print(integer[i] + ", ");
+            } else {
+                System.out.println(integer[i]);
+            }
         }
         System.out.print("Дробный массив drob: ");
-        for (int i = 0; i < drob.length; i++)
-        {
-            if (i < drob.length - 1)
-            {
+        for (int i = 0; i < drob.length; i++) {
+            if (i < drob.length - 1) {
                 System.out.print(drob[i] + ", ");
-            }else
-            {
+            } else {
                 System.out.println(drob[i]);
             }
         }
         System.out.print("Строчный массив str: ");
-        for (int i = 0; i < str.length; i++)
-        {
-            if (i < str.length - 1)
-            {
+        for (int i = 0; i < str.length; i++) {
+            if (i < str.length - 1) {
                 System.out.print(str[i] + ", ");
-            }else
-            {
+            } else {
                 System.out.println(str[i]);
             }
+        }
+        // Задание 3
+        System.out.println("================= Задание 3 ==============");
+        System.out.print("Целочисленный массив integer: ");
+        int i = integer.length-1;
+        while (i >= 0) {
+            if (i == 0)
+            {
+                System.out.println(integer[i]);
+            }else
+            {
+                System.out.print(integer[i] + ", ");
+            }
+            i--;
+        }
+        System.out.print("Дробный массив drob: ");
+        i = drob.length-1;
+        while (i >= 0) {
+            if (i == 0)
+            {
+                System.out.println(drob[i]);
+            }else
+            {
+                System.out.print(drob[i] + ", ");
+            }
+            i--;
+        }
+        System.out.print("Строковый массив str: ");
+        i = str.length-1;
+        while (i >= 0) {
+            if (i == 0)
+            {
+                System.out.println(str[i]);
+            }else
+            {
+                System.out.print(str[i] + ", ");
+            }
+            i--;
         }
     }
 }
